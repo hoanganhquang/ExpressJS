8819bf149c078e4e2fcae782f9b89b3763fcd5ad
hoanganhquang <anhquang976@gmail.com>
2021-09-03 22:28:24 +0700

    updated project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 const express = require('express')
+const exphbs = require("express-handlebars");
 const path = require('path')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 // HTTP Logger
 app.use(morgan('combined'))
 
 //Load view engine
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('home')
 })
 
-app.get('/info', (req, res) => {
-    res.render('info')
-})
 
 app.listen(port, () =>{
     console.log(`Listening at http://localhost:${port}`)
