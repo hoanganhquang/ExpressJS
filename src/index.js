6efa38cd41e6175f8c3a4961e8107e212314a68d
hoanganhquang <anhquang976@gmail.com>
2021-09-04 11:42:33 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const exphbs = require("express-handlebars");
 const path = require('path')
-const morgan = require('morgan')
+const morgan = require('morgan');
+const { join } = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+// use static file
+app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP Logger
 app.use(morgan('combined'))
@@ -15,9 +17,10 @@ app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'))
 
+
 app.get('/', (req, res) => {
     res.render('home')
-})
+})  
 
 
 app.listen(port, () =>{
