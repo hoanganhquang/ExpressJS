db1ca60982e28de0d1b3428cd72d9ec66b3ff7d8
hoanganhquang <anhquang976@gmail.com>
2021-09-14 15:00:43 +0700

    update project

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const port = process.env.PORT;
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => console.log("DB connection successful"))
+  .catch((err) => console.log(err));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "Must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
