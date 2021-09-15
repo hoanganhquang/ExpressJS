f83c69e0b6d7d5a16c0064b477833c68e5eb9c28
hoanganhquang <anhquang976@gmail.com>
2021-09-15 16:29:57 +0700

    update project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,37 @@
 const Tour = require("../models/tourModel");
 
-exports.getAllTours = (req, res, next) => {
-  res.status(200).json({
-    data: "dataTour",
-  });
+exports.getAllTours = async (req, res, next) => {
+  try {
+    const allTour = await Tour.find();
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        allTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+    });
+  }
 };
 
-exports.getTour = (req, res, next) => {
-  res.status(200).json({
-    data: "Tour1",
-  });
+exports.getTour = async (req, res, next) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
 };
 
 exports.createTour = async (req, res, next) => {
@@ -29,3 +51,38 @@ exports.createTour = async (req, res, next) => {
     });
   }
 };
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: error,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res)=>{
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+      status: "Success"
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail"
+    })
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,9 +3,11 @@ const {
   getAllTours,
   getTour,
   createTour,
+  updateTour,
+  deleteTour,
 } = require("../controllers/tourController");
 
 tourRoutes.route("/").get(getAllTours).post(createTour);
-tourRoutes.route("/:id").get(getTour);
+tourRoutes.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = tourRoutes;
