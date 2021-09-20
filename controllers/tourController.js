67a4ce6a50852dd575db7d0f3ca1339fe25e1fe4
hoanganhquang <anhquang976@gmail.com>
2021-09-20 17:04:58 +0700

    update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -202,7 +202,59 @@ exports.getTourStats = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
+    res.status(400).json({
+      status: "Fail",
+      message: error,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: { _id: 0 },
+      },
+      {
+        $sort: {
+          numToursStarts: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        plan,
+      },
+    });
+  } catch (error) {
     res.status(400).json({
       status: "Fail",
       message: error,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,9 +7,11 @@ const {
   deleteTour,
   aliasTopTour,
   getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController");
 
 tourRoutes.route("/tour-stats").get(getTourStats);
+tourRoutes.route("/monthly-plan/:year").get(getMonthlyPlan);
 tourRoutes.route("/top-5-cheap").get(aliasTopTour, getAllTours);
 tourRoutes.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 tourRoutes.route("/").get(getAllTours).post(createTour);
