a323033d5c8f1986399673d556c2fa7b89981be2
hoanganhquang <anhquang976@gmail.com>
2021-09-24 23:08:21 +0700

    update

--- controllers/errController.js
+++ controllers/errController.js
@@ -1,9 +1,34 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      error: err,
+      message: err.message,
+    });
+  } else {
+    console.log("Errorr", err);
+    res.status(500).json({
+      status: "err",
+      message: "sth went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "Error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, send);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
